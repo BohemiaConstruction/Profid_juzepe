e725eb4f8f90037163c43972f844db3df2df2134
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-01-15 22:06:08 +0100

    oprava

--- project_task_unit_reporting/models/project_task.py
+++ project_task_unit_reporting/models/project_task.py
@@ -14,9 +14,11 @@ class ProjectTask(models.Model):
         for task in self:
             task.total_unit_qty = sum(task.timesheet_ids.mapped("unit_qty"))
 
-    def action_close_task(self):
-        res = super(ProjectTask, self).action_close_task()
+    def write(self, vals):
+        res = super(ProjectTask, self).write(vals)
         for task in self:
-            if task.sale_line_id:
-                task.sale_line_id.qty_delivered += task.total_unit_qty
+            if "total_unit_qty" in vals or "timesheet_ids" in vals:
+                if task.sale_line_id:
+                    # Update delivered quantity dynamically
+                    task.sale_line_id.qty_delivered = task.total_unit_qty
         return res
