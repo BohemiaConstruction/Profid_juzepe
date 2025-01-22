60d21f9352db3d611911c4a578a00ce4fa1ecba8
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-01-22 09:26:11 +0100

    Revert na puvodní

--- xf_replace_email_params/models/mail_message.py
+++ xf_replace_email_params/models/mail_message.py
@@ -1,27 +1,30 @@
-
 from odoo import models, api
 
 
 class MailMessage(models.Model):
     _inherit = 'mail.message'
 
+    def get_author_user(self, author_partner_id):
+        if not author_partner_id:
+            return
+        partner = self.env['res.partner'].with_context(active_test=False).browse(author_partner_id)
+        if partner and partner.user_ids:
+            for user in partner.user_ids:
+                return user
+
     @api.model_create_multi
     def create(self, values_list):
         for values in values_list:
             author_partner_id = values.get('author_id', False)
             model = values.get('model', False)
-            res_id = values.get('res_id', False)
-            user = self.env['res.users'].search([('partner_id', '=', author_partner_id)], limit=1)
-            company = user.company_id if user else self.env.company
-            internal_user = user.has_group('base.group_user') if user else False
-
-            email_from, reply_to = self.env['mail.replace.rule'].get_email_from_reply_to(
-                model, company, internal_user, record_id=res_id
-            )
-
+            user = self.get_author_user(author_partner_id)
+            company = user and user.company_id
+            internal_user = user and user.has_group('base.group_user')
+            email_from, reply_to = self.env['mail.replace.rule'].get_email_from_reply_to(model, company, internal_user)
             if email_from:
-                values['email_from'] = email_from
-            if reply_to:
-                values['reply_to'] = reply_to
-
+                # Overwrite email_from if replacement exists
+                values.update({'email_from': email_from})
+            if reply_to is not None:
+                # Overwrite reply_to if replacement exists
+                values.update({'reply_to': reply_to})
         return super(MailMessage, self).create(values_list)
