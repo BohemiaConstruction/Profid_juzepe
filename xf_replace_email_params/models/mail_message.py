695e8d9c88a17a7b2e897abf622c7db8be9cbf7d
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-01-29 22:21:32 +0100

    Update mail_message.py

--- xf_replace_email_params/models/mail_message.py
+++ xf_replace_email_params/models/mail_message.py
@@ -1,4 +1,7 @@
-from odoo import models, api
+from odoo import models, api, fields
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class MailMessage(models.Model):
     _inherit = 'mail.message'
@@ -14,53 +17,16 @@ class MailMessage(models.Model):
     @api.model_create_multi
     def create(self, values_list):
         for values in values_list:
-            message_type = values.get('message_type', '')
-            domain = [
-                ('model', '=', values.get('model')),
-                ('company_id', '=', values.get('company_id')),
-                ('only_for_internal_users', '=', values.get('only_for_internal_users'))
-            ]
-            if message_type:
-                domain.append(('message_type_filter', '=', message_type))
-            
-            rules = self.env['mail.replace.rule'].search(domain)
-            for rule in rules:
-                if rule.domain_filter:
-                    try:
-                        filter_condition = eval(rule.domain_filter)
-                        if not isinstance(filter_condition, dict):
-                            raise ValidationError("Domain filter must be a valid dictionary, e.g., {'support_team': 1}")
-                        
-                        for field, value in filter_condition.items():
-                            if 'res_id' in values:
-                                related_record = self.env[values.get('model')].browse(values.get('res_id'))
-                                field_value = getattr(related_record, field, None)
-                                if isinstance(field_value, models.BaseModel):
-                                    field_value = field_value.id  
-                                _logger.info(f"Checking {field} (value: {field_value}) against {value}")
-                                
-                                if field_value != value:
-                                    _logger.info(f"Field {field} does not match filter. Skipping update.")
-                                    break
-                            else:
-                                _logger.warning(f"res_id not found in values: {values}")
-                                continue
-                        else:
-                            _logger.info(f"Filter matched, updating emails for: {values}")
-                            email_from, reply_to = rule.get_email_from_reply_to(values.get('model'), values.get('company_id'), values.get('only_for_internal_users'))
-                            if email_from:
-                                values.update({'email_from': email_from})
-                            if reply_to is not None:
-                                values.update({'reply_to': reply_to})
-                    except Exception as e:
-                        _logger.error(f"Error applying filter: {e}")
-                        raise ValidationError(f"Invalid filter condition: {e}")
-                else:
-                    _logger.info(f"No domain filter set. Updating emails for: {values}")
-                    email_from, reply_to = rule.get_email_from_reply_to(values.get('model'), values.get('company_id'), values.get('only_for_internal_users'))
-                    if email_from:
-                        values.update({'email_from': email_from})
-                    if reply_to is not None:
-                        values.update({'reply_to': reply_to})
-        
+            author_partner_id = values.get('author_id', False)
+            model = values.get('model', False)
+            user = self.get_author_user(author_partner_id)
+            company = user and user.company_id
+            internal_user = user and user.has_group('base.group_user')
+            email_from, reply_to = self.env['mail.replace.rule'].get_email_from_reply_to(model, company, internal_user)
+            if email_from:
+                # Overwrite email_from if replacement exists
+                values.update({'email_from': email_from})
+            if reply_to is not None:
+                # Overwrite reply_to if replacement exists
+                values.update({'reply_to': reply_to})
         return super(MailMessage, self).create(values_list)
