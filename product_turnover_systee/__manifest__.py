a66d02bac52219b9bfee2580fe940aa4ee6cee65
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-02-23 15:58:30 +0100

    ppp

--- product_turnover_systee/__manifest__.py
+++ product_turnover_systee/__manifest__.py
@@ -5,12 +5,6 @@
     'data': [
         'views/product_template_view.xml',
     ],
-    'assets': {
-        'web.assets_backend': [
-            'product_turnover_systee/static/lib/chart.bundle.min.js',
-            'product_turnover_systee/static/src/js/product_turnover.js',
-        ],
-    },
     'installable': True,
     'application': False,
 }
--- product_turnover_systee/controllers/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import product_turnover_controller
