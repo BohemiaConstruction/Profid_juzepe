89248947889f88a965d2dc87b4d44caa583682d2
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-02-23 15:59:27 +0100

    Update __manifest__.py

--- product_turnover_systee/__manifest__.py
+++ product_turnover_systee/__manifest__.py
@@ -1,7 +1,15 @@
 {
     'name': 'Product Turnover Systee',
     'version': '1.0',
+<<<<<<< HEAD
     'depends': ['base', 'web', 'sale'],
+=======
+    'summary': 'Calculates product turnover based on past sales with prediction',
+    'category': 'Sales',
+    'author': 'Your Name',
+    'depends': ['sale', 'stock'],
+    'external_dependencies': {'python': ['numpy']},
+>>>>>>> parent of b392049 (Update __manifest__.py)
     'data': [
         'views/product_template_view.xml',
     ],
