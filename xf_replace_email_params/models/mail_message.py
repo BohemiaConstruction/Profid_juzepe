65bd77f4ea5ba7108d48543d49f66af1c7ff1c34
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-02-27 20:09:42 +0100

    oprava funkcnosti domenoveho filtru

--- xf_replace_email_params/models/mail_message.py
+++ xf_replace_email_params/models/mail_message.py
@@ -14,8 +14,6 @@ class MailMessage(models.Model):
             for user in partner.user_ids:
                 return user
 
-    
-    
     @api.model_create_multi
     def create(self, values_list):
         messages = super(MailMessage, self).create(values_list)
@@ -25,6 +23,10 @@ class MailMessage(models.Model):
             for rule in rules:
                 if rule.message_type_filter and rule.message_type_filter != message.message_type:
                     continue
+
+                # Pokud domain_filter není vyplněn, nebo odpovídá, pravidlo se aplikuje
+                apply_rule = not rule.domain_filter
+
                 if rule.domain_filter:
                     try:
                         filter_condition = eval(rule.domain_filter)
@@ -32,20 +34,32 @@ class MailMessage(models.Model):
                             _logger.warning("Domain filter must be a valid dictionary, e.g., {'support_team': 1}")
                             continue
 
+                        apply_rule = True
                         for field, value in filter_condition.items():
                             related_record = message.env[message.model].browse(message.res_id)
                             field_value = getattr(related_record, field, None)
                             if isinstance(field_value, models.BaseModel):
                                 field_value = field_value.id  
                             if field_value != value:
+                                apply_rule = False
                                 break
-                        else:
-                            if rule.block_sending:
-                                _logger.info(f"Blocking email sending for message: {message.id}")
-                                message.sudo().write({'state': 'cancel'})  # Nastaví zprávu jako zrušenou
+
                     except Exception as e:
                         _logger.error(f"Error applying filter: {e}")
                         continue
 
-        return messages
+                # Pokud pravidlo platí, aplikujeme změny emailu
+                if apply_rule:
+                    email_from, reply_to = rule.email_from_computed, rule.reply_to_computed
+                    if email_from:
+                        message.sudo().write({'email_from': email_from})
+                    if reply_to:
+                        message.sudo().write({'reply_to': reply_to})
 
+                    # Pokud je aktivní block_sending, zpráva nebude odeslána
+                    if rule.block_sending:
+                        _logger.info(f"Blocking email sending for message: {message.id}")
+                        message.sudo().write({'state': 'cancel'})
+
+        return messages
+    
