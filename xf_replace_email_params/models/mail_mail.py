226994213462a6e081f95db89a727483db7ac08d
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-02-27 20:47:45 +0100

    vycisteni

--- xf_replace_email_params/models/mail_mail.py
+++ xf_replace_email_params/models/mail_mail.py
@@ -14,28 +14,12 @@ class MailMail(models.Model):
                 rules = self.env["mail.replace.rule"].search([])
 
                 for rule in rules:
-                    if rule.message_type_filter and rule.message_type_filter != message.message_type:
-                        continue
-                    if rule.domain_filter:
-                        try:
-                            filter_condition = eval(rule.domain_filter)
-                            if not isinstance(filter_condition, dict):
-                                _logger.warning("Domain filter must be a valid dictionary, e.g., {'support_team': 1}")
-                                continue
+                    # NEAPLIKUJEME domain_filter zde, protože už byl aplikován v mail.message
 
-                            for field, value in filter_condition.items():
-                                related_record = message.env[message.model].browse(message.res_id)
-                                field_value = getattr(related_record, field, None)
-                                if isinstance(field_value, models.BaseModel):
-                                    field_value = field_value.id  
-                                if field_value != value:
-                                    break
-                            else:
-                                if rule.block_sending:
-                                    _logger.info(f"Blocking email sending for mail: {values.get('mail_message_id')}")
-                                    values["state"] = "cancel"  # Zrušíme e-mail
-                        except Exception as e:
-                            _logger.error(f"Error applying filter: {e}")
-                            continue
+                    if rule.block_sending:
+                        _logger.info(f"Blocking email sending for mail: {values.get('mail_message_id')}")
+                        values["state"] = "cancel"  # Zrušíme e-mail
+                        break  # Ukončíme kontrolu, protože blokování je dostatečné
 
         return super(MailMail, self).create(values_list)
+    
