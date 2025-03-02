9dcaf16d7a5cb3204b58b5fdf4b6f0ce50cc64c4
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-02 09:08:36 +0100

    oprava

--- product_properties/models/product_property.py
+++ product_properties/models/product_property.py
@@ -30,3 +30,14 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     property_id = fields.Many2one('product.property', string='Technical Properties', ondelete='cascade')
+
+    # Related fields
+    property_category_id = fields.Many2one('product.property.category', related='property_id.category_id', string="Property Category", store=True)
+    part_number = fields.Char(related='property_id.part_number', string="Part Number", store=True)
+    value = fields.Float(related='property_id.value', string="Value", store=True)
+    unit = fields.Selection(related='property_id.unit', string="Unit", store=True)
+    voltage_rating_vdc = fields.Float(related='property_id.voltage_rating_vdc', string="Voltage Rating [VDC]", store=True)
+    dielectric = fields.Selection(related='property_id.dielectric', string="Dielectric", store=True)
+    tolerance = fields.Float(related='property_id.tolerance', string="Tolerance [%]", store=True)
+    footprint = fields.Selection(related='property_id.footprint', string="Footprint", store=True)
+    note = fields.Text(related='property_id.note', string="Note", store=True)
