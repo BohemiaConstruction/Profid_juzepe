48a5be988d484728eb737df58afdd09d25cb5246
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-13 00:29:46 +0100

    [MIG] product_properties: Migration to 18.0

--- product_properties/__manifest__.py
+++ product_properties/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Product Properties Simplified',
-    'version': '1.3',
+    'version': "18.0.1.0.0",
     'category': 'Product',
     'summary': 'Adds technical properties to products without extra scripts',
     'author': 'Custom Developer',
