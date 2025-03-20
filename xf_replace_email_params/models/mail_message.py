e9fb3f898dd725ad9ae4a35d0e9c93820d1d44cd
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-20 15:00:57 +0100

    Update mail_message.py

--- xf_replace_email_params/models/mail_message.py
+++ xf_replace_email_params/models/mail_message.py
@@ -19,7 +19,8 @@ class MailMessage(models.Model):
     @api.model_create_multi
     def create(self, values_list):
         new_values_list = []
-        _logger.warning(f"Nová zpráva vytvořena - Model: {values.get('model', 'Neznámý')}, Message Type: {values.get('message_type', 'Neznámý')}")
+            for values in values_list:
+                _logger.warning(f"Nová zpráva vytvořena - Model: {values.get('model', 'Neznámý')}, Message Type: {values.get('message_type', 'Neznámý')}")
         for values in values_list:
             author_partner_id = values.get('author_id', False)
             model = values.get('model', False)
