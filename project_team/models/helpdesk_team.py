ace76eae7e8a0a05e4c764c52b19c449aa77324b
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-23 10:59:06 +0100

    Update helpdesk_team.py

--- project_team/models/helpdesk_team.py
+++ project_team/models/helpdesk_team.py
@@ -4,23 +4,23 @@ from odoo import models, fields, api
 class HelpdeskTeam(models.Model):
     _inherit = 'helpdesk.team'
 
-    project_team_id = fields.Many2one('project.team', string="Project Team", ondelete='set null')
+    project_team_id = fields.Many2one('crm.team', string="Project Team", domain="[('type_team','=','project')]", ondelete='set null')
 
     @api.onchange('project_team_id')
     def _onchange_project_team_id(self):
         if self.project_team_id:
-            self.member_ids = [(6, 0, self.project_team_id.member_ids.ids)]
+            self.member_ids = [(6, 0, self.project_team_id.team_members_ids.ids)]
 
     def write(self, vals):
         res = super().write(vals)
         if 'project_team_id' in vals and vals['project_team_id']:
             for team in self:
-                team.member_ids = [(6, 0, team.project_team_id.member_ids.ids)]
+                team.member_ids = [(6, 0, team.project_team_id.team_members_ids.ids)]
         return res
 
     @api.model
     def create(self, vals):
         team = super().create(vals)
         if vals.get('project_team_id'):
-            team.member_ids = [(6, 0, team.project_team_id.member_ids.ids)]
+            team.member_ids = [(6, 0, team.project_team_id.team_members_ids.ids)]
         return team
