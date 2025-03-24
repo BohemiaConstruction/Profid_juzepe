e27fb17d8f80b66ba53ab8e2706ed3379ea7c309
Josef Dostal <32773664+Juzepe1@users.noreply.github.com>
2025-03-24 08:04:58 +0100

    oprava multi

--- project_team/models/helpdesk_ticket.py
+++ project_team/models/helpdesk_ticket.py
@@ -1,17 +1,20 @@
 from odoo import models, api
 
 
+from odoo import models, api
+
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
-    @api.model
-    def create(self, vals):
-        if vals.get('partner_id'):
-            partner = self.env['res.partner'].browse(vals['partner_id'])
-            if partner.project_team_id:
-                team = self.env['helpdesk.team'].search([
-                    ('project_team_id', '=', partner.project_team_id.id)
-                ], limit=1)
-                if team:
-                    vals['team_id'] = team.id  # přepíše i pokud je vyplněno
-        return super().create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('partner_id'):
+                partner = self.env['res.partner'].browse(vals['partner_id'])
+                if partner.project_team_id:
+                    team = self.env['helpdesk.team'].search([
+                        ('project_team_id', '=', partner.project_team_id.id)
+                    ], limit=1)
+                    if team:
+                        vals['team_id'] = team.id  # přepíše i pokud je vyplněno
+        return super().create(vals_list)
